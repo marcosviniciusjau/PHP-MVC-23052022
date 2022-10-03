34ccf2b3d94138c3128af656fee7dc17a671d6c3
marcosviniciusjau <marcosviniciusjahu@gmail.com>
2022-10-03 11:47:33 -0300

    configuração das rotas

--- App/rotas.php
+++ App/rotas.php
@@ -3,12 +3,24 @@
 use App\Controller\PessoaController;
 use App\Controller\ProdutoController;
 use App\Controller\ProdutoCategoriaController;
-
+use App\Controller\LoginController;
 
 $url = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
 
 switch ($url) {
+    case '/login':
+        LoginController::index();
+    break;
+
+    case '/login/auth':
+        LoginController::auth();
+    break;
+
+    case '/logout':
+        LoginController::logout();
+    break;
+
     case '/':
         echo "página inicial";
         break;
